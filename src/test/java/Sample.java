999fb927655cdb0af97f043d96e9d6df7d90edce
moganraj <moganpondy@gmail.com>
2018-07-26 01:32:25 -0400

    chk2

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,8 +1,10 @@
+import junit.framework.Assert;
 
 public class Sample {
 public static void main(String[] args) {
 	for(int i =1; i<=100; i++){
 		System.out.println(i);
 	}
+	Assert.assertEquals(true, true);
 }
 }
